e282281a1085758eb1272955dcf4f748daf1f514
alexlc <alex.lu@fox-ess.com>
2026-03-13 16:38:09 +0800

    Update typo.

--- llm/rag/rag_basic_demo.py
+++ llm/rag/rag_basic_demo.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# os.putenv('OPENAI_API_KEY', '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')
 
 print(os.getenv('OPENAI_API_KEY'))
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'), base_url="https://api.chatanywhere.tech")
