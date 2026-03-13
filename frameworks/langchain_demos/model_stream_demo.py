a9386dd671425865be7be81e8808bd459b10cc20
alexlc <alex.lu@fox-ess.com>
2026-03-14 00:49:10 +0800

    Update typo.

--- frameworks/langchain_demos/model_stream_demo.py
+++ frameworks/langchain_demos/model_stream_demo.py
@@ -3,9 +3,12 @@ import langchain.chat_models
 from langchain_classic.chat_models import init_chat_model
 from openai import base_url
 import asyncio
+from langchain_openai import ChatOpenAI
 
-model = init_chat_model(model='gpt-5.1', api_key=os.getenv('OPENAI_API_KEY'),
-                        base_url='https://api.chatanywhere.tech')
+# model = init_chat_model(model='gpt-5.1', api_key=os.getenv('OPENAI_API_KEY'),
+#                         base_url='https://api.chatanywhere.tech')
+model = ChatOpenAI(api_key=os.getenv('DASHSCOPE_API_KEY'),model='qwen-max',
+                   base_url="https://dashscope.aliyuncs.com/compatible-mode/v1")
 # Simple streaming
 # full = None
 # for chunk in model.stream('What color is the sky?'):
@@ -35,7 +38,15 @@ list_of_inputs = [
     'What is quantum computing?'
 ]
 # Limit to 5 parallel calls
-responses = model.batch_as_completed(list_of_inputs, config={'max_concurrency': 5})
+# responses = model.batch_as_completed(list_of_inputs, config={'max_concurrency': 5})
+#
+# for resp in responses:
+#     print(resp)
 
-for resp in responses:
-    print(resp)
+# Reasoning demo
+# for chunk in model.stream('What is the meaning of life?'):
+#     reasoning_steps = [r for r in chunk.content_blocks if r['type'] == 'reasoning']
+#     print(reasoning_steps if reasoning_steps else chunk.text)
+
+response = model.invoke("Create a picture of a cat")
+print(response.content_blocks)
